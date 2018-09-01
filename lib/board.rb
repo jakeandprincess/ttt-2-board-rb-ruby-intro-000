f0a72178edf89db7e27dab567ccd7d8a81b5c96e
christy.hatherley@yahoo.com <christy.hatherley@yahoo.com>
2018-09-01 13:37:06 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,7 @@
 # Define the variable board below.
 board = [" "," "," "," "," "," "," "," "," "]
 def hello_world_ten_times
-  hello world
-  hello world
-  hello world
+  puts "hello world"
+  puts "hello world"
+  puts "hello world"
 end
